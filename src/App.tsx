811a62e84b3c1517552c06300211fe73ffdc381a
anikasujana <anikasujana03@gmail.com>
2025-03-17 23:05:03 -0400

    Added survey form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@mantine/core/styles.css';
+import SurveyForm from './components/SurveyForm';
+import {MantineProvider} from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MantineProvider>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+
+      <div>
+        <SurveyForm />
+      </div>
+    </MantineProvider>
   )
 }
 
