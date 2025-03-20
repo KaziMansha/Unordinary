4b4bf7a60dc9a1a1df0812717ce225a2276b5548
KaziMansha <kazi.mansha2@gmail.com>
2025-03-20 03:18:28 -0400

    Navbar completed

--- src/home/Home.page.tsx
+++ src/home/Home.page.tsx
@@ -1,6 +1,11 @@
+import classes from './Home.module.css'
+import { NavBar } from './components/Navbar/Navbar.tsx'
+
+
 export function HomePage() {
     return (
         <>
+        <NavBar />
         <h1>
             Temp Code
         </h1>
---
+++ src/home/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './Navbar.module.css'
+import Unordinary_Logo from '../../assets/Unordinary_Logo.png'
+
+export function NavBar() {
+    return (
+        <>
+        <nav className = {classes.navbar}>
+            <div className = {classes.logo}>
+                <img src = {Unordinary_Logo} alt = "Unordinary Logo" />
+            </div>
+            <ul className = {classes.navLinks}>
+                <li>
+                    <Link to="*">Dashboard</Link>
+                </li>
+                <li>
+                    <Link to="*">Profile</Link>
+                </li>
+                <li>
+                    <Link to="*">About</Link>
+                </li>
+            </ul>
+            <div className = {classes.authButtons}>
+                <Link to="*"><button className={classes.login}>Login</button></Link>
+                <Link to="*"><button className={classes.login}>Sign Up</button></Link>
+            </div>
+        </nav>
+        </>
+    )
+}
