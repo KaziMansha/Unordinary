d413181d30a08ba3a67a32f364395ad3d468b57d
KaziMansha <mmansha93@gmail.com>
2025-04-01 23:28:14 -0400

    Pushing Changes to pull commit

--- src/dashboard/components/Calendar/Calendar.tsx
+++ src/dashboard/components/Calendar/Calendar.tsx
@@ -5,7 +5,29 @@ export function Calendar() {
         <>
             <div className={classes.calendar}>
                 <div className={'$classes.month-calendar $classes.month-calendar--five-week'}>
-                    
+                   <ul className={'$classes.month-calendar__day-of-week-list'}>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Sunday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Monday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Tuesday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Wednesday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Thursday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Friday'}
+                        </li>
+                        <li className={'$classes.month-calendar__day-of-week'}>
+                            {'Saturday'}
+                        </li>
+                   </ul>
                 </div>
             </div>
         </>
